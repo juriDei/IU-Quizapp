1ec2070d7c37cad4db0d7f5ac8925dfbe15dc217
juriDei <juri.deisling@iubh-fernstudium.de>
2024-10-23 14:20:22 +0200

    Add files via upload

--- controller/DBConnection.php
+++ controller/DBConnection.php
@@ -1,24 +1,33 @@
 <?php 
 
+// Verwenden von PDO und PDOException, um die Datenbankverbindung zu verwalten
 use PDO;
 use PDOException;
 
+// Klasse zur Verwaltung der Datenbankverbindung
 class DBConnection{
     
+    // Zeichensatzdefinition für die Datenbankverbindung
     private static $charset = 'utf8mb4';
+    
+    // Optionen für die PDO-Verbindung, um das Verhalten der Fehlerbehandlung und des Fetch-Modus zu konfigurieren
     private static $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Fehler im Ausnahme-Modus behandeln
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Ergebnisse als assoziative Arrays abrufen
+        PDO::ATTR_EMULATE_PREPARES => false, // Keine Emulation vorbereiteter Anweisungen verwenden
     ];
 
+    // Stellt eine Verbindung zur Datenbank her und gibt eine PDO-Instanz zurück
     public static function getDBConnection() : PDO{
+        // DSN (Data Source Name) für die Verbindung mit der Datenbank zusammensetzen
         $dsn = "mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME') . ";charset=" . self::$charset;
         try {
+            // PDO-Instanz erstellen und Verbindung zur Datenbank herstellen
             $dbh = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'), self::$options);
-            return $dbh;
+            return $dbh; // Datenbankverbindung zurückgeben
         } catch (PDOException $e) {
-            // Hier kannst du entscheiden, ob du den Fehler protokollieren, weiterleiten oder einfach nur eine Nachricht anzeigen möchtest
+            // Fehler abfangen, wenn die Verbindung fehlschlägt, und eine Fehlermeldung ausgeben
+            // Hier könntest du auch den Fehler protokollieren, um ihn zu überwachen
             die("Verbindung zur Datenbank fehlgeschlagen: " . $e->getMessage());
         }
     }
